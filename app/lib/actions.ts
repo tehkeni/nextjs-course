2eac68535f22f6d5bb7f3809fd27a3d4718ce6be
tehkeni <keniteh@gmail.com>
2024-06-20 23:54:58 +0800

    implement errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -65,7 +65,8 @@ export async function createInvoice(formData: FormData) {
   }
 
   export async function deleteInvoice(id: string) {
-
+    throw new Error('Failed to Delete Invoice');
+    
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
