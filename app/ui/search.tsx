cd2f2093cc44986ba961ffffb8c8fbd0be253dec
tehkeni <keniteh@gmail.com>
2024-06-20 09:57:52 +0800

    install debounce and implement

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
  
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -9,6 +10,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
   
   function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+    
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -16,6 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
+  }, 300);
   }
  
   return (
