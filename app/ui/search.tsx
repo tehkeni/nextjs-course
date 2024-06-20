2d94fb060dfa5ca11a217077a128178e18c6af33
tehkeni <keniteh@gmail.com>
2024-06-20 10:08:28 +0800

    fix debounce for search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,10 +9,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   
-  function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
-    
+   
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -21,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
-  }
+  
  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
