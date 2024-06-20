1194fc78ea4716877c7a924714cfee5f34e02423
tehkeni <keniteh@gmail.com>
2024-06-20 09:36:48 +0800

    enable ex

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+ 
+const nextConfig = {
+    experimental: {
+      ppr: 'incremental',
+    },
+  };
+   
+  module.exports = nextConfig;
